7d6d7e5f92f0a93065cd53ec36496cca5c0c6cd7
wangzhe409 <Doctor@DESKTOP-BSF3655>
2018-03-17 13:24:23 +0800

    添加项目文件。

---
+++ ZL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZL.Startup))]
+namespace ZL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
